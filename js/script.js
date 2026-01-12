2c9e71cd59211cc082ce93c4d42201e6554f2726
Sumit Das <sumit@example.com>
2026-01-12 16:38:12 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,93 @@
+// =======================================================
+
+var namtaString = "5";
+var nam = "This is java script language class 01";
+var nam1 = true;
+console.log( namtaString*6);
+console.log( nam);
+console.log( nam1);
+
+// =========================================================
+// =========================================================
+const pi = 3.14;
+console.log( pi);
+// =========================================================
+a =5;
+b =10;
+console.log( a+b);
+
+{
+    let a = 20;
+    let b = 30;
+    console.log( a+b);
+}
+{
+    var a = 20;
+    var b = 30;
+  
+}
+  console.log( a+b);
+
+
+   var abc = "Hello World";
+let def = 5;
+var hij =true;
+console.log(typeof abc);
+console.log(typeof def);
+console.log(typeof hij);
+
+let number = 5;
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*1);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*2);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*3);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*4);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*5);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*6);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*7);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*8);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*9);
+console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*10);  
+
+
+$('.slider').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  prevArrow: '<button type="button" class="absolute top-[-80px] right-[80px]  z-10 w-[45px] h-[45px] cursor-pointer bg-primary7 text-primary5 rounded-full hover:bg-primary1 hover:duration-500 transition-all hover:text-white"><i class="fa-solid fa-arrow-left"></i></button>',
+  nextArrow: '<button type="button" class="absolute top-[-80px] right-[0px]  z-10 w-[45px] h-[45px] cursor-pointer bg-primary7 text-primary5 rounded-full hover:bg-primary1 hover:duration-500 transition-all hover:text-white"><i class="fa-solid fa-arrow-right"></i></button>'
+});
+$('.slider3').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  prevArrow: '<button type="button" class="absolute top-[-80px] right-[80px]  z-10 w-[45px] h-[45px] cursor-pointer bg-primary7 text-primary5 rounded-full hover:bg-primary1 hover:duration-500 transition-all hover:text-white"><i class="fa-solid fa-arrow-left"></i></button>',
+  nextArrow: '<button type="button" class="absolute top-[-80px] right-[0px]  z-10 w-[45px] h-[45px] cursor-pointer bg-primary7 text-primary5 rounded-full hover:bg-primary1 hover:duration-500 transition-all hover:text-white"><i class="fa-solid fa-arrow-right"></i></button>'
+});
+$('.slider1').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false
+  
+});
+$('.slider2').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false
+  
+});
+$('.slider4').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false
+  
+});
